32bbf6f840c5c590951a7bc03ca6ac23f05a82d9
Yusuf <yusufcodes@outlook.com>
2022-06-19 11:18:38 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Yusuf on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
