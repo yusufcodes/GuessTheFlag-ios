104250100dcd81dad91a2926e454bf8acc0967fa
Yusuf <yusufcodes@outlook.com>
2022-06-19 13:20:43 +0100

    feat: initial logic and UI for guessing the flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        .shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    
+    func checkAnswer(userSelection: Int) -> Bool {
+        countries[userSelection] == countries[correctAnswer] ? true : false
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack(spacing: 10) {
+                    Text("Tap the correct flag for: ")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text((countries[correctAnswer]))
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                VStack {
+                    ForEach(0..<3) { number in
+                        Button {
+                            showingAlert = true
+                            let isAnswerCorrect = checkAnswer(userSelection: number)
+                            scoreTitle = isAnswerCorrect ? "Correct!" : "Incorrect"
+                            if (isAnswerCorrect) {
+                                score += 1
+                            }
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                        }
+                        .alert(scoreTitle, isPresented: $showingAlert) {
+                            Button("Play Again", action: askQuestion)
+                        } message: {
+                            Text("Your score is \(score)")
+                        }
+                        .clipShape(Capsule())
+                        .shadow(radius: 5)
+                    }
+                }
+            }
+        }
     }
 }
 
